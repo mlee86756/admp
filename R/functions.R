a38da76195c644db7da5935d1e81ddccbc034e08
b7042166 <b7042166@my.shu.ac.uk>
2019-03-19 23:08:29 +0000

    limited progress with songkick API queries

--- R/functions.R
+++ R/functions.R
@@ -2,3 +2,38 @@
 
 library(dplyr)
 
+
+
+# MUSICBRAINZ UDFs
+
+artist_name_to_songkick_id <- function(artist_name){
+  mb_gid <- artist_name_to_musicbrainz_gid(artist_name)
+  result <- musicbrainz_gid_to_songkick_id(mb_gid)
+  Sys.sleep(0.5)
+  return(result)
+}
+
+artist_name_to_musicbrainz_gid <- function(artist_name) {
+  library(jsonlite)
+  # build url for musicbrainz api query with artist search
+  json_url_for_mb_gid <- URLencode(paste('http://musicbrainz.org/ws/js/artist?limit=1&q=',artist_name,sep=""))
+  # execute musicbrainz api query
+  json1 <- head(fromJSON(json_url_for_mb_gid),1)
+  # retrieve gid from musicbrainz data
+  return(json1$gid)
+}
+
+musicbrainz_gid_to_songkick_id <- function(musicbrainz_gid) {
+  library(jsonlite)
+  # building url for further musicbrainz api query to include 'relationship' URLs
+  json_url_for_links <- paste('http://musicbrainz.org/ws/2/artist/', musicbrainz_gid, '?inc=url-rels', sep="")
+  # execute second musicbrainz api query
+  json2 <- fromJSON(json_url_for_links)
+  
+  # extract URLs 
+  youtube_url <- json2$relations$url$resource[json2$relations$type=='youtube']
+  official_url <- json2$relations$url$resource[json2$relations$type=='official homepage']
+  songkick_url <- json2$relations$url$resource[json2$relations$type=='songkick']
+  # extract songkick ID from songkick URL
+  return(basename(songkick_url)) 
+}
--- R/musicbrainz_json.R
+++ R/musicbrainz_json.R
@@ -1,29 +1,6 @@
 library(httr)
 library(jsonlite)
 
-# UDF  to retrieve songkick id from musicbrainz
-get_songkick_id <- function(artist_name) {
-  # build url for musicbrainz api query with artist search
-  json_url_for_mb_gid <- URLencode(paste('http://musicbrainz.org/ws/js/artist?limit=1&q=',artist_name,sep=""))
-  json_url_for_mb_gid
-  # execute musicbrainz api query
-  json1 <- head(fromJSON(json_url_for_mb_gid),1)
-  # retrieve gid from musicbrainz data
-  mb_gid <- json1$gid
-  
-  # building url for further musicbrainz api query to include 'relationship' URLs
-  json_url_for_links <- paste('http://musicbrainz.org/ws/2/artist/', mb_gid, '?inc=url-rels', sep="")
-  # execute second musicbrainz api query
-  json2 <- fromJSON(json_url_for_links)
-  
-  # extract URLs 
-  youtube_url <- json2$relations$url$resource[json2$relations$type=='youtube']
-  official_url <- json2$relations$url$resource[json2$relations$type=='official homepage']
-  songkick_url <- json2$relations$url$resource[json2$relations$type=='songkick']
-  Sys.sleep(0.5)
-  # extract songkick ID from songkick URL
-  return(basename(songkick_url))
-}
 
 # import spotify dataset
 spotify <- read.csv("Excel/spotify.csv")
@@ -37,5 +14,13 @@ top_20 <- head(artist_plays[order(artist_plays$x, decreasing=TRUE),], 20)
 rownames(top_20) <- 1:20
 top_20_artists <- top_20[1]
 
-# lapply to get songkick IDs
-songkick_ids <- lapply(top_20_artists$Artist, get_songkick_id)
+# lapply to get songkick IDs (RUN FUNCTIONS IN functions.R)
+songkick_ids <- lapply(top_20_artists$Artist, artist_name_to_songkick_id)
+
+# trying songkick query (results need decoding...)
+songkick_api_key <- "OdCeFTr8qFUSwUVt"
+songkick_artist_id <- artist_name_to_songkick_id("Eminem")
+songkick_query_url <- paste ("https://api.songkick.com/api/3.0/artists/", 
+  songkick_artist_id, "/gigography.json?apikey=", songkick_api_key, sep="")
+library(jsonlite)
+songkick_results <- fromJSON(songkick_query_url)
